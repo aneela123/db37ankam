e72928df82429accb9ce53d695b39f16e40ceb3d
aneela123 <77805472+aneela123@users.noreply.github.com>
2021-04-09 13:13:42 -0500

    My initial commit

--- controllers/shoe.js
+++ controllers/shoe.js
@@ -12,9 +12,17 @@ exports.shoe_list = async function (req, res) {
     // res.send('NOT IMPLEMENTED: shoe list');
 };
 // for a specific shoe.
-exports.shoe_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: shoe detail: ' + req.params.id);
+exports.shoe_detail = async function(req, res) {
+    console.log("detail"  + req.params.id)
+    try {
+        result = await Shoe.findById( req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
 };
+
 // Handle shoe create on POST.
 exports.shoe_create_post = async function (req, res) {
     console.log(req.body)
@@ -40,10 +48,27 @@ exports.shoe_create_post = async function (req, res) {
 exports.shoe_delete = function (req, res) {
     res.send('NOT IMPLEMENTED: shoe delete DELETE ' + req.params.id);
 };
-// Handle shoe update form on PUT.
-exports.shoe_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: shoe update PUT' + req.params.id);
+// Handle Shoe update form on PUT.
+exports.shoe_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await Shoe.findById( req.params.id)
+        // Do updates of properties
+        if(req.body.shoename) toUpdate.shoename = req.body.shoename;
+        if(req.body.type) toUpdate.type = req.body.type;
+        if(req.body.color) toUpdate.color = req.body.color;
+        if(req.body.price) toUpdate.price = req.body.price;
+
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
+    }
 };
+
+
 
 // VIEWS
 // Handle a show all view
